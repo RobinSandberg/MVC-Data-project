852831dc2551455c3af51090b2c9924995b080bd
robin sandberg <robinsandberglex@outlook.com>
2018-05-07 16:28:40 +0200

    init

---
+++ MVC Data project/Controllers/PeopleController.cs
@@ -0,0 +1,66 @@
+﻿using MVC_Data_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Data_project.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View(Person.DbPeople);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                Person.DbPeople.Add(person);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(person);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Person person = Person.DbPeople.SingleOrDefault(i => i.Id == id);
+            Person.DbPeople.Remove(person);
+            
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id)
+        {
+            Person person = Person.DbPeople.SingleOrDefault(i => i.Id == id);
+            Person.DbPeople.Add(person);
+
+            return RedirectToAction("Index");
+        }
+
+        
+        public ActionResult Details()
+        {
+            return View(Person.DbPeople);
+        }
+
+        public ActionResult Hide()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC Data project/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Data_project.Models
+{
+    public class Person
+    {
+        static int idCount = 0;
+        public static List<Person> DbPeople = new List<Person>();
+
+        [Key]
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string City { get; set; }
+
+        public Person()
+        {
+            Id = idCount++;
+        }
+    }
+}
